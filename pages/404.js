55117b8824ed22ad0b999b62df59bbe13107d802
Emabliss <emmajmv@gmail.com>
2021-11-26 12:45:42 +0000

    initial

---
+++ pages/404.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const NotFound = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, [router]);
+
+  return (
+    <div className="not-found">
+      <h1>Oooops....</h1>
+      <p>That page cannot be found.</p>
+      <p>
+        Go back to the{" "}
+        <Link href="/">
+          <a>Homepage</a>
+        </Link>{" "}
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
--- pages/about.js
+++ pages/about.js
@@ -1,35 +1,29 @@
 import Head from "next/head";
-import Image from "next/image";
-import Navbar from "../comps/Navbar";
-import Footer from "../comps/Footer";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
 
-export default function Home() {
+const About = () => {
   return (
     <>
       <Head>
-        <title>Ninja List | Home</title>
+        <title>Ninja List | About</title>
         <meta name="keywords" content="ninja" />
       </Head>
       <div>
-        <h1 className={styles.title}>Homepage</h1>
-        <p className={styles.text}>
+        <h1>About</h1>
+        <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Et deleniti
           error asperiores, beatae mollitia minus iusto. Earum enim corporis
           cumque ullam id nulla doloremque, blanditiis illum voluptates tempore
           perferendis accusamus!
         </p>
-        <p className={styles.text}>
+        <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Et deleniti
           error asperiores, beatae mollitia minus iusto. Earum enim corporis
           cumque ullam id nulla doloremque, blanditiis illum voluptates tempore
           perferendis accusamus!
         </p>
-        <Link href="/ninjas">
-          <a className={styles.btn}>See Ninja Listings</a>
-        </Link>
       </div>
     </>
   );
-}
+};
+
+export default About;
